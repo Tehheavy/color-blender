73c50138593e443cb9a5290cb5e94aa49594b716
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-11-04 01:03:49 +0200

    Updates

---
+++ precache-manifest.428a646309664ddccfb1c03d1667aea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d616d532178ce8190abb747fb0ddb01a",
+    "url": "/color-blender/index.html"
+  },
+  {
+    "revision": "109b4833c46c190ad4f3",
+    "url": "/color-blender/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "263835315b2b9081ff50",
+    "url": "/color-blender/static/css/main.8126cdba.chunk.css"
+  },
+  {
+    "revision": "109b4833c46c190ad4f3",
+    "url": "/color-blender/static/js/2.d0664021.chunk.js"
+  },
+  {
+    "revision": "263835315b2b9081ff50",
+    "url": "/color-blender/static/js/main.8aef4ed0.chunk.js"
+  },
+  {
+    "revision": "1c8ebf309be2fc406e56",
+    "url": "/color-blender/static/js/runtime-main.f3dd2ec9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/color-blender/static/media/logo.25bf045c.svg"
+  }
+]);
